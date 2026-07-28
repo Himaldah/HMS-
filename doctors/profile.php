32847fb23cab7b6e7c830287aea2cc3a4fdbd98b
Himaldah <161953463+Himaldah@users.noreply.github.com>
2026-07-28 22:30:31 +0545

    Update profile header text  in profile.php

--- doctors/profile.php
+++ doctors/profile.php
@@ -10,7 +10,7 @@ $doctor = mysqli_fetch_assoc($res);
 
 <main class="p-6 max-w-3xl mx-auto pt-20">
     <div class="bg-white rounded-lg shadow p-6 hover:shadow-lg hover:shadow-blue-200 transition duration-300">
-        <h2 class="text-2xl font-bold text-blue-700 mb-4">My Profile</h2>
+        <h2 class="text-2xl font-bold text-blue-700 mb-4"> Mero Profile </h2>
         <p class = "text-gray-700 mb-4"><strong>Doctor ID:</strong> <?php echo htmlspecialchars($doctor['drid']); ?></p>
         <p class = "text-gray-700 mb-4"><strong>Department ID:</strong> <?php echo htmlspecialchars($doctor['did']); ?></p>
         <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 text-gray-700">
