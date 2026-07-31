ea891f00e8112afcf718ff6e759ebd3a4b5f4243
Panday2024 <142554187+Panday2024@users.noreply.github.com>
2026-07-31 23:25:42 +0545

    Updates payments.php to improve code structure .

--- admin/payments.php
+++ admin/payments.php
@@ -1,4 +1,20 @@
+<?php
+$pageTitle = 'Payments   ';
+include 'includes/sidemenu.php';
+include 'includes/header.php';  
+include 'api/admin_notification.php';
 
+
+$payments_query = "SELECT * FROM payments ORDER BY pmcreated_at DESC";
+$result = $conn->query($payments_query);
+
+if (isset($_GET["delete"])) {
+    $id = $_GET["delete"];
+    $conn->query("DELETE FROM payments WHERE pmid=$id");
+    header("Location: payments.php");
+    $_SESSION['toast'] = ['message' => 'Payment has been deleted.', 'type' => 'success'];
+} 
+?>
 
         <main class="flex-1 ml-52 p-2 overflow-auto mt-14">
         <div class="bg-white shadow-md rounded-lg p-6">
