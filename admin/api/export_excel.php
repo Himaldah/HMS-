95a46f024cafd6eef1e0c292a3bc8fb6932d5b54
Panday2024 <142554187+Panday2024@users.noreply.github.com>
2026-08-16 22:09:36 +0545

    Add excel export functionality for patients and doctors with improved data output results.

--- admin/api/export_excel.php
+++ admin/api/export_excel.php
@@ -1,4 +1,46 @@
-{
+<?php
+
+$page_title = $_POST['pagetitle'];
+
+$host = "localhost";  // Change if using a remote server
+$username = "root";   // Change to your MySQL username
+$password = "";       // Change to your MySQL password
+$database = "hsm_db"; // Change to your MySQL database name
+
+
+// Connect to database
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if($page_title == "Patients/Users"){
+
+    header("Content-Type: application/vnd.ms-excel");
+    header("Content-Disposition: attachment; filename=patient_exported_data.xls");
+
+    echo "<table border='1'>";
+    echo "<tr><td colspan='8' style='text-align: center; font-weight: bold;'>$page_title</td></tr>";
+    echo "<tr><th>ID</th><th>Created At</th><th>Name</th><th>Gender</th><th>DOB</th><th>Phone</th><th>Address</th><th>Email</th></tr>";
+
+    $qry = mysqli_query($conn, "SELECT * FROM patients");
+    while($row = mysqli_fetch_assoc($qry)) {
+        echo "<tr>";
+        echo "<td>" . $row['pid'] . "</td>";
+        echo "<td>" . $row['pcreated_at'] . "</td>";
+        echo "<td>" . $row['pname'] . "</td>";
+        echo "<td>" . $row['pgender'] . "</td>";
+        echo "<td>" . $row['pdob'] . "</td>";
+        echo "<td>" . $row['pphone'] . "</td>";
+        echo "<td>" . $row['paddress'] . "</td>";
+        echo "<td>" . $row['pemail'] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+
+} elseif($page_title == "Doctors") {
 
     header("Content-Type: application/vnd.ms-excel");
     header("Content-Disposition: attachment; filename=appointments_exported_data.xls");
